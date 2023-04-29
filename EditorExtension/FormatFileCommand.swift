a58bfc9e13c20abd1a5311e9ddf80dedcd7e9514
Shx Guo <int123c@gmail.com>
2023-04-29 21:19:15 +0800

    Support in-place format

--- EditorExtension/FormatFileCommand.swift
+++ EditorExtension/FormatFileCommand.swift
@@ -8,23 +8,49 @@ class FormatFileCommand: NSObject, XCSourceEditorCommand {
     ) {
         let uti = invocation.buffer.contentUTI
         let content = invocation.buffer.completeBuffer
+        let lines = invocation.buffer.lines
         do {
             let service = try getService()
-            service.formatFile(content: content, uti: uti, completionHandler: {
-                switch $0 {
-                case let .failure(error):
-                    completionHandler(error)
-                case let .success(content):
-                    let selectionsRangesToRestore = invocation.buffer.selections
-                        .compactMap { $0 as? XCSourceTextRange }
-                    invocation.buffer.selections.removeAllObjects()
-                    invocation.buffer.completeBuffer = content
-                    for range in selectionsRangesToRestore {
-                        invocation.buffer.selections.add(range)
+            service.formatFile(
+                content: content,
+                lines: lines as! [String],
+                uti: uti,
+                completionHandler: {
+                    switch $0 {
+                    case let .failure(error):
+                        completionHandler(error)
+                    case let .success(diff):
+                        let selectionsRangesToRestore = invocation.buffer.selections
+                            .compactMap { $0 as? XCSourceTextRange }
+                        guard let diff,
+                              let nsmutableStringDiff = CollectionDifference<Any>(
+                                  diff.map {
+                                      switch $0 {
+                                      case let .insert(offset, element, associatedWith):
+                                          return .insert(
+                                              offset: offset,
+                                              element: NSMutableString(string: element),
+                                              associatedWith: associatedWith
+                                          )
+                                      case let .remove(offset, element, associatedWith):
+                                          return .remove(
+                                              offset: offset,
+                                              element: NSMutableString(string: element),
+                                              associatedWith: associatedWith
+                                          )
+                                      }
+                                  }
+                              )
+                        else {
+                            completionHandler(nil)
+                            return
+                        }
+
+                        invocation.buffer.lines.apply(nsmutableStringDiff)
+                        completionHandler(nil)
                     }
-                    completionHandler(nil)
                 }
-            })
+            )
         } catch {
             completionHandler(error)
         }
